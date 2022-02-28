c9bf86a59ca31a7715fc5d5f72fdba4a8a5d143c
Tamas Schell <tanksapka@yahoo.com>
2022-02-28 23:08:19 +0100

    Adding ProductScreen component, context manager and cookie to handle dark mode switch.

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import NextLink from "next/link";
 import Layout from "../components/Layout";
 import { Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Grid, Typography } from "@material-ui/core";
 import data from "../utils/data";
@@ -11,12 +12,14 @@ export default function Home() {
           {data.products.map((product) => (
             <Grid item md={4} key={product.name}>
               <Card>
-                <CardActionArea>
-                  <CardMedia component="img" image={product.image} title={product.name}></CardMedia>
-                  <CardContent>
-                    <Typography>{product.name}</Typography>
-                  </CardContent>
-                </CardActionArea>
+                <NextLink href={`/product/${product.slug}`} passHref>
+                  <CardActionArea>
+                    <CardMedia component="img" image={product.image} title={product.name}></CardMedia>
+                    <CardContent>
+                      <Typography>{product.name}</Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </NextLink>
                 <CardActions>
                   <Typography>${product.price}</Typography>
                   <Button size="small" color="primary">
